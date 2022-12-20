c32d85a71776f9d83f2d5c0100d2d12c2cd0e52a
ProninaAnastasiia <82177320+ProninaAnastasiia@users.noreply.github.com>
2022-12-20 22:33:48 +0300

    init commit

---
+++ serviceone/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private HttpClient _client;
+    public HomeController()
+    {
+        _client = new HttpClient();
+        var uri = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["uri"];
+        _client.BaseAddress = new Uri(uri);
+        _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        
+    }
+
+    [HttpGet("test")]
+    public IActionResult Test()
+    {
+        return Ok("Single works!");
+    }
+
+    [HttpGet("network")]
+    public IActionResult NetworkRequest()
+    {
+        var response = _client.GetAsync("WeatherForecast").Result;
+        return Ok(response.Content.ReadAsStringAsync().Result);
+    }
+}
